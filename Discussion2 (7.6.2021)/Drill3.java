64eb168b5ff2bac1dfa4201949ea88fa36a173bc
Sachin Deshpande <35118726+sachincdeshpande@users.noreply.github.com>
2021-07-06 14:48:21 -0700

    Add files via upload

---
+++ Discussion2 (7.6.2021)/Drill3.java
@@ -0,0 +1,24 @@
+class TextTweet {
+    /* some fields*/
+    String contents;
+    int likes;
+    /* some methods*/
+    hasMention()
+    hasLike(change something here)
+    firstMention()
+}
+
+class ReplyTweet {
+    /* some fields*/
+    /* some methods*/
+    morePopularReply()
+    allLikes()
+    hasMention()
+}
+
+
+public class Drill3 {
+    /* Tester class */  
+    TextTweet testTweet = new TextTweet("hi", 0);
+    boolean isLiked = testTweet.hasLikes();  
+}
